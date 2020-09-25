b9526b06e3192c61ceea12fb14ef640150cde483
TakenakaRoberta <takenaka.roberta@gmail.com>
2020-09-25 20:00:51 -0300

    Padroniza o nome completo, removendo os espaços em branco entre os nomes e colocando os nomes em letras maiúsculas

--- app/planilhas.py
+++ app/planilhas.py
@@ -3,5 +3,18 @@ def padronizar_nome_completo(nome_completo_despadronizados):
     """
     Padroniza um nome completo:
     - removendo os espaços no início e no fim
+    - removendo espaços extras entre os nomes
+    - colocando em maiúsculas, inclusive nomes acentuados
     """
-    return nome_completo_despadronizados.strip()
+    padronizado = nome_completo_despadronizados.strip()
+    padronizado = padronizado.upper()
+    while True:
+        padronizado = padronizado.replace(" "*2, " ")
+        if "  " not in padronizado:
+            break
+    return padronizado
+
+
+if __name__ == "__main__":
+    nome = padronizar_nome_completo("Roberta")
+    print(nome)
